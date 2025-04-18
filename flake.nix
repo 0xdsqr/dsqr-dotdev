c24a668f359d66dc9d2ce6d3ffdf7b7a1dc1bbfa
DaveVED <dave.w.dennis@gmail.com>
2025-04-18 13:56:03 -0500

    feat: add in flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "🟪 dsqr-dotdev 🟪";
+  
+  inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  
+  outputs = { self, nixpkgs-unstable, nixpkgs-stable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs-unstable = import nixpkgs-unstable { inherit system; };
+        pkgs-stable = import nixpkgs-stable { inherit system; };
+      in {
+        devShells.default = pkgs-unstable.mkShell {
+          buildInputs = with pkgs-unstable; [
+            git
+          ];
+          
+          shellHook = ''
+            echo "🟪 development environment is ready..."
+          '';
+        };
+      }
+}
