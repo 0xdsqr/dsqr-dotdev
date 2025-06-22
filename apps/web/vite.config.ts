a2a7407775e214db602e2c6fd37f79f6a88d34cc
DaveVED <me@dsqr.dev>
2025-06-21 20:57:20 -0500

    feat: standing back up the foundation

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -13,6 +13,17 @@ export default defineConfig({
     react(), 
     tailwindcss()
   ],
+  server: {
+      host: '0.0.0.0',
+      port: 5173,
+      allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '192.168.50.223',
+      'dev-dsqr.dev',
+      '.dev-dsqr.dev'
+    ],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
