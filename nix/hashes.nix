99f28ebdb27725abe2178e99f2785421b57f020f
0xdsqr <me@dsqr.dev>
2026-04-28 15:41:30 -0500

    chore: replace package build script with tsc

--- nix/hashes.nix
+++ nix/hashes.nix
@@ -1,6 +1,6 @@
 {
   nodeModules = {
-    "aarch64-darwin" = "sha256-UocFQPLkAb0YKHotnLlEg9jw3XsMENrdXxgxo88jTLk=";
-    "x86_64-linux" = "sha256-UocFQPLkAb0YKHotnLlEg9jw3XsMENrdXxgxo88jTLk=";
+    "aarch64-darwin" = "sha256-gu72+oi72ZRhU7P8cDsT70v7W7it5sagl3V7biXNtME=";
+    "x86_64-linux" = "sha256-gu72+oi72ZRhU7P8cDsT70v7W7it5sagl3V7biXNtME=";
   };
 }
